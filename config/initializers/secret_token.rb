d41ee712438ba1bcda2ac8d567ba3f7c38ae67f3
Yuzhe <yuzhe.c85@gmail.com>
2012-08-06 16:34:42 +0800

    第一次

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jasonhazel::Application.config.secret_token = 'db5d120b4a06eb1d2ae9642931f6430fef7b818f9883f1dac38f6f6f9ae2618f90c2c728e07add2c99e351862dc0217c00559af52fe16c7aa4464315ebc277d6'
